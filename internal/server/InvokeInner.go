abb1ecd2ba7db859d266f62ba8d39b6d9aee8ffc
MXuDong <2941884109@qq.com>
2021-05-23 12:03:18 +0800

    Add inner invoke funcs

---
+++ internal/server/InvokeInner.go
@@ -0,0 +1,13 @@
+package server
+
+/**
+For invoke inner with application.
+
+The inner invoker support some method to control the response of the http.
+But now, not support the https(without any test or usage).
+
+For invoke, it provide json params to control next invoke response. So any invoke
+can use this to compose the complex system trace.
+
+- @author: MXuDong
+*/
